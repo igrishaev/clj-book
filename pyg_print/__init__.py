e0e728d0299458153b0fc54c955efeffa9311f24
Shipilov Aleksei <alexkontext@yandex.ru>
2020-06-22 12:32:55 +0300

    init pygments style package

---
+++ pyg_print/__init__.py
@@ -0,0 +1,35 @@
+
+# https://github.com/pygments/pygments/blob/master/pygments/styles/bw.py
+# https://github.com/pygments/pygments/blob/48e55050f8bec34d57a1a86e06e779c6288b8341/pygments/lexers/jvm.py#L736
+
+from pygments.style import Style
+from pygments.token import Keyword, Name, Comment, String, Error, \
+     Number, Operator, Generic
+
+GRAY = "#444"
+LIGHTGRAY = "#eee"
+
+class PrintStyle(Style):
+    default_style = "bw"
+    styles = {
+
+        # HTTP
+        Name.Attribute:         GRAY,
+        Keyword.Reserved:       "nobold",
+
+        # JSON
+        Name.Tag:               GRAY,
+
+        # diff
+        Generic.Deleted:        "bg:" + LIGHTGRAY,
+        Generic.Inserted:       "bg:",
+
+        # Clojure
+        Keyword:                "bold",
+        Generic.Emph:           "noitalic",
+        String:                 "nobold",
+        String.Symbol:          GRAY,
+        Comment:                "noitalic " + GRAY,
+        Name.Function:          "bold",
+
+    }
