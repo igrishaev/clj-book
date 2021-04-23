4a2f3ba8eb16133ae7684ad090cb9fba662c33d5
Uunnamed <uunnamed@mail.ru>
2021-04-23 13:28:34 +0300

    fix #70

--- check-urls.sh
+++ check-urls.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
-grep \footurl *.tex |
-while read -r row ; do
-    file=$(echo $row | grep -o '\w*-\w*.tex')
-    url=$(echo $row | grep -Eo 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)' | sed -e "s/\\\//g")
+RED='\033[0;31m';
+NC='\033[0m';
+
+grep '}{http' *.tex |
+while read row ; do
+    file=$(echo $row | grep -o '\w*.tex')
+    url=$(echo $row | sed -e "s/\\\//g" | pcregrep -Mo 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
+    if [ "$url" == "" ];
+    then
+       continue;
+    fi
     urlstatus=$(curl -o  /dev/null --connect-timeout 7 --silent --head -L --write-out '%{http_code}' "$url" )
     echo "$url  $urlstatus"
     if [ $urlstatus -ne 200 ];
     then
-        echo "Error status for url: $url in file: $file"
-        exit 1;
+        echo "${RED}Error status for url: $url in file: $file ${NC}"
+        continue;
     fi
 done
